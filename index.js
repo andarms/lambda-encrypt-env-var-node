76351e7475beb707e5c0010d6a2d7be5692648b3
Adrian Manjarres <amanja4@bancodebogota.com.co>
2021-05-07 11:04:08 -0500

    read secrets

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { decryptSecret } = require("./secret-encrypt");
 exports.handler = async (event) => {
   const secret1 = await decryptSecret("var01");
   const secret2 = await decryptSecret("var02");
-  const arms = await decryptSecret("var02");
+  const arms = await decryptSecret("ARMS");
 
   return `var: '${secret1}' var 2: '${secret2}', text from secrets:  ARMS: ${arms}`;
 };
