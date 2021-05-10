73dbcd71057ed6b26b4f418528fc435f81ebfa8a
Adrian Manjarres <amanja4@bancodebogota.com.co>
2021-05-10 10:24:09 -0500

    remove decryption from not secrets vars

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { decryptSecret } = require("./secret-encrypt");
 
 exports.handler = async (event) => {
-  const secret1 = await decryptSecret("var01");
-  const secret2 = await decryptSecret("var02");
+  const secret1 = process.env.var01;
+  const secret2 = process.env.var01;
   const arms = await decryptSecret("ARMS");
 
   return `var: '${secret1}' var 2: '${secret2}', text from secrets:  ARMS: ${arms}`;
